dfa1b3f0ac7991ab1ed8bc78a412efa1f17c861a
Mareike Täubner <mareike.taeubner@procilon.de>
2019-05-19 16:59:05 +0200

    styled header

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,10 @@ import { ReactComponent as Logo } from "./icons/baseline-translate-24px.svg";
 
 function Header() {
   return (
-    <div className="header">
-      <Logo />
+    <header>
+      <Logo className="logo" />
       <div>Sy'k Fiar translator</div>
-    </div>
+    </header>
   );
 }
 
